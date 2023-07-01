dc16249e36eb427f10cc0fc46c3d300257c6acac
vlok31 <33446843+vlok31@users.noreply.github.com>
2023-07-01 13:25:27 +0200

    Update streamlit_app.py
    add return to button function

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,7 +49,7 @@ streamlit.header("The fruit load list contains:")
 def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
         my_cur.execute("select * from fruit_load_list")
-        my_cur.fetchall()
+        return my_cur.fetchall()
 
 # Add a button to 4load the fruit
 if streamlit.button('Get Fruit Load List'):
